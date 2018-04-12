0b4fb636afc9bf43b072b67d53c7a5cc76787322
zeelee28 <32179557+zeelee28@users.noreply.github.com>
2018-04-12 15:38:48 -0700

    display state with JSX

--- wesbos-react/catch-of-the-day/src/components/App.js
+++ wesbos-react/catch-of-the-day/src/components/App.js
@@ -2,11 +2,14 @@ import React from 'react'
 import Header from './Header'
 import Order from './Order'
 import Inventory from './Inventory'
+import Fish from './Fish'
+import sampleFishes from "../sample-fishes"
 
 class App extends React.Component{
     constructor(){
         super()
         this.addFish = this.addFish.bind(this)
+        this.loadSamples = this.loadSamples.bind(this)        
         //getinitialstate
         this.state={
             fishes: {},
@@ -23,15 +26,27 @@ class App extends React.Component{
         //set state
         this.setState({fishes})
     }
+    loadSamples(){
+        this.setState({
+            fishes: sampleFishes
+        })
+    }
     render(){
         return(
             <div className="catch-of-the-day">
                 <div className='menu'> 
                     {/*if you want to pass information to a tag, pass it via Props*/}
                     <Header tagline="Fresh Seafood Restaurant" />
+                    <ul className='list-of-fishes'>
+                        {
+                            Object
+                                .keys(this.state.fishes)
+                                .map(key=><Fish key={key} details={this.state.fishes[key]} />)
+                        }
+                    </ul>
                 </div>
                 <Order />
-                <Inventory addFish={this.addFish} />
+                <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
             </div>
         )
     }
