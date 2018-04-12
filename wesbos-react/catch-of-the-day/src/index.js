2b5e366a9fd90bca63840e7a5501da1bddb68da3
zeelee28 <32179557+zeelee28@users.noreply.github.com>
2018-04-12 10:18:24 -0700

    make my first component

--- wesbos-react/catch-of-the-day/src/index.js
+++ wesbos-react/catch-of-the-day/src/index.js
@@ -1,1 +1,9 @@
-// let's go!
+// let's go!--you can code all your script tags for plug-ins here!!
+// need a module bundler (webpack) to import all libraries: react/react-dom etc.
+
+import React from 'react' // import everything from react library into React variable
+import {render} from 'react-dom' // import render method from react-dom package
+
+import StorePicker from './components/StorePicker'
+
+render(<StorePicker/>, document.querySelector('#main')) // use JSX to render out component
