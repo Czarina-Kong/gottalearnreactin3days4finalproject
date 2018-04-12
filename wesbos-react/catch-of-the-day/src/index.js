45727292050693e6651cc8cc4288089dd666251b
zeelee28 <32179557+zeelee28@users.noreply.github.com>
2018-04-12 11:01:22 -0700

    create app layout with components

--- wesbos-react/catch-of-the-day/src/index.js
+++ wesbos-react/catch-of-the-day/src/index.js
@@ -3,8 +3,9 @@
 
 import React from 'react' // import everything from react library into React variable
 import {render} from 'react-dom' // import render method from react-dom package
-import './css/style.css'
-
+import './css/style.css' // take everything inside file and webpack will compile and pop style tag on it
+// if you create separate css file for each Component, scope css to single Component
+import App from './components/App'
 import StorePicker from './components/StorePicker'
 
-render(<StorePicker/>, document.querySelector('#main')) // use JSX to render out component
+render(<App/>, document.querySelector('#main')) // use JSX to render out component
