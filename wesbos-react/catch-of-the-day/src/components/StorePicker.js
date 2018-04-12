d9a22b4ce42b524a5598ec4afef6000db8e1b058
zeelee28 <32179557+zeelee28@users.noreply.github.com>
2018-04-12 12:09:31 -0700

    code events

--- wesbos-react/catch-of-the-day/src/components/StorePicker.js
+++ wesbos-react/catch-of-the-day/src/components/StorePicker.js
@@ -2,11 +2,25 @@ import React from 'react'
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component{
+    // looks for goToStore method and sets itself to itself and binds it to this
+    // constructor(){
+    //     super()
+    //     this.goToStore=this.goToStore.bind(this)
+    // }
+
+    goToStore(event){ // add additional method to react component
+        event.preventDefault()
+        console.log('You changed the url')
+        // first grab text from box
+        console.log(this.storeInput.value) // this does not automatically equal the component 
+        // second transition from / to /store/:storeID
+    }
+
     render(){
         return (
-            <form className="store-selector"> 
+            <form className="store-selector" onSubmit={this.goToStore.bind(this)}> 
                 <h2>Please Enter A Store</h2>
-                <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+                <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input)=>{this.storeInput=input}} />
                 <button type='submit'>Visit Store</button>
                 {/*this is how you write comments in JSX*/}
             </form> // JSX--can only every return one parent element
