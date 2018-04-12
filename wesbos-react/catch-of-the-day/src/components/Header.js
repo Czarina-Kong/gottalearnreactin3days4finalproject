55889614c827317cdb8c1a99c115d159d12f37a8
zeelee28 <32179557+zeelee28@users.noreply.github.com>
2018-04-12 11:29:54 -0700

    convert component to stateless functional component

--- wesbos-react/catch-of-the-day/src/components/Header.js
+++ wesbos-react/catch-of-the-day/src/components/Header.js
@@ -1,22 +1,44 @@
 import React from 'react'
 
-class Header extends React.Component{
-    render(){
-        return(
-            <header className='top'>
-                <h1>
-                    Catch
-                    <span className="ofThe">
-                        <span className="of">of</span>
-                        <span className="the">the</span>
-                    </span>
-                    Day
-                </h1>
-                <h3 className="tagline"><span>{this.props.tagline}</span></h3>
-                {/*one set of curly brackets in JSX inserts variables*/}
-            </header>
-        )
-    }
+// class Header extends React.Component{
+//     render(){
+//         return(
+//             <header className='top'>
+//                 <h1>
+//                     Catch
+//                     <span className="ofThe">
+//                         <span className="of">of</span>
+//                         <span className="the">the</span>
+//                     </span>
+//                     Day
+//                 </h1>
+//                 <h3 className="tagline"><span>{this.props.tagline}</span></h3>
+//                 {/*one set of curly brackets in JSX inserts variables*/}
+//             </header>
+//         )
+//     }
+// }
+// 
+// Let's make the above code, a Stateless Functional Component
+// if you don't need other methods other than render (like render HTML to DOM), use SFC
+// 
+
+const Header=(props)=>{
+    return(
+        <header className='top'>
+            <h1>
+                Catch
+                <span className="ofThe">
+                    <span className="of">of</span>
+                    <span className="the">the</span>
+                </span>
+                Day
+            </h1>
+            <h3 className="tagline"><span>{props.tagline}</span></h3>
+            {/*one set of curly brackets in JSX inserts variables*/}
+        </header>
+    )
 }
+
 
 export default Header
