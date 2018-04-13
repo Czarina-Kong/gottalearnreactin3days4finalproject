f573272c5139018a50df20b19232114b764c6500
zeelee28 <32179557+zeelee28@users.noreply.github.com>
2018-04-12 17:46:32 -0700

    persist state with firebase

---
+++ wesbos-react/catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base'
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyBuwiSTFC0MnDiq2QEeEe7Op5KUzlMhrnM",
+    authDomain: "catch-of-the-day-cjl-9b619.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-cjl-9b619.firebaseio.com"
+})
+
+export default base
